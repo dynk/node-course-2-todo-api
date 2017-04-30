ffd90b1ad415aa08d41047f146d62a1710fb4cac
Raphael Salvador <rsalvador1@luc.edu>
2017-04-30 13:24:42 -0300

    add patch /todos/:id

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -12,6 +12,10 @@ const {User} = require('./../server/models/user');
 // Todo.findOneAndRemove
 // Todo.findByIdAndRemove
 
+Todo.findOneAndRemove({_id: '59060391d779ee487b767322'}).then((todo)=>{
+
+});
+
 Todo.findByIdAndRemove('59060391d779ee487b767322').then((todo)=>{
     console.log(todo);
 })
