ae937ad77cc180c4ca25f41aa2da87e326d75f1f
Raphael Salvador <rsalvador1@luc.edu>
2017-05-13 19:10:26 -0300

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,29 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id:10
-};
+var password = '123abc!';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     })
+// })
+
+var hashedPassoword = '$2a$10$MNPYaGsAhBC4xYaYaSEpluMdpIpkhdODqDvONBmuHtkyMIVJryN9m'
+
+bcrypt.compare("password", hashedPassoword, (err, res)=>{
+    console.log(res);
+})
+// var data = {
+//     id:10
+// };
 
 
-var token = jwt.sign(data, '123abc')
-console.log(token)
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded)
+// var token = jwt.sign(data, '123abc')
+// console.log(token)
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded)
 
 
 
--- server/models/user.js
+++ server/models/user.js
@@ -2,35 +2,36 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs')
 
 
 var UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 1,
-    unique: true,
-    validate: {
-      validator: validator.isEmail,
-      message: '{VALUE} is not a valid email'
-    }
-  },
-  password: {
-    type: String,
-    require: true,
-    minlength: 6
-  },
-  tokens: [{
-    access: {
-      type: String,
-      required: true
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
     },
-    token: {
-      type: String,
-      required: true
-    }
-  }]
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
 UserSchema.methods.toJSON = function () {
@@ -43,9 +44,9 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {
     var user = this;
     var access = 'auth';
-    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+    var token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
 
-    user.tokens.push({access, token});
+    user.tokens.push({ access, token });
     return user.save().then(() => {
         return token;
     });
@@ -55,10 +56,10 @@ UserSchema.statics.findByToken = function (token) {
     var User = this;
     var decoded;
 
-    try{
+    try {
         decoded = jwt.verify(token, 'abc123');
         console.log(decoded)
-    }catch(e){
+    } catch (e) {
         return Promise.reject();
     }
 
@@ -70,6 +71,21 @@ UserSchema.statics.findByToken = function (token) {
 
 };
 
+UserSchema.pre('save', function (next) {
+    var user = this;
+
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            })
+        })
+    } else {
+        next();
+    }
+});
+
 var User = mongoose.model('User', UserSchema);
 
-module.exports = {User}
+module.exports = { User }
