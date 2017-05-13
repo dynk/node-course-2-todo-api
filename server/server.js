8af8feea42709d4f746ea2dfa2a85a68dd5d5027
Raphael Salvador <rsalvador1@luc.edu>
2017-05-13 18:41:25 -0300

    add get /users/me and authenticate middleware

--- server/server.js
+++ server/server.js
@@ -5,11 +5,12 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const {ObjectID} = require('mongodb');
+const { ObjectID } = require('mongodb');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
+var {authenticate} = require('./middleware/authenticate')
 
 var app = express();
 var port = process.env.PORT;
@@ -17,72 +18,72 @@ var port = process.env.PORT;
 app.use(bodyParser.json());
 
 
-app.post('/todos',(req, res)=>{
+app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
     })
-    todo.save().then((doc)=>{
+    todo.save().then((doc) => {
         res.send(doc);
-    }, (e)=>{
+    }, (e) => {
         res.status(400).send(e);
     })
 });
 
-app.get('/todos',(req, res)=>{
-    Todo.find().then((todos)=>{
-        res.send({todos})
-    }, (e) =>{
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos })
+    }, (e) => {
         res.status(400).send(e);
     })
 })
 
-app.get('/todos/:id', (req,res) => {
+app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
-    if(!ObjectID.isValid(id)){
+    if (!ObjectID.isValid(id)) {
         return res.status(404).send("Id not valid ");
     }
-    Todo.findById(id).then((todo)=>{
-        if(!todo) return res.status(404).send('No todo found')
-        res.send({todo});
+    Todo.findById(id).then((todo) => {
+        if (!todo) return res.status(404).send('No todo found')
+        res.send({ todo });
     })
 
 })
 
-app.delete('/todos/:id', (req,res)=>{
-    
+app.delete('/todos/:id', (req, res) => {
+
     var id = req.params.id;
-    if(!ObjectID.isValid(id)) return res.status(404).send("Id not valid");
+    if (!ObjectID.isValid(id)) return res.status(404).send("Id not valid");
 
-    Todo.findByIdAndRemove(id).then((todo)=>{
-        if(!todo) return res.status(404).send('Todo not found')
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) return res.status(404).send('Todo not found')
 
-        res.send({todo});
-    }).catch((e)=>{
+        res.send({ todo });
+    }).catch((e) => {
         res.status(400).send();
     })
 
 });
 
-app.patch('/todos/:id', (req,res) => {
+app.patch('/todos/:id', (req, res) => {
     var id = req.params.id;
-    var body = _.pick(req.body,['text', 'completed']);
+    var body = _.pick(req.body, ['text', 'completed']);
 
-    if(!ObjectID.isValid(id)){
+    if (!ObjectID.isValid(id)) {
         return res.status(404).send("Id not valid ");
     }
 
-    if(_.isBoolean(body.completed) && body.completed){
+    if (_.isBoolean(body.completed) && body.completed) {
         body.completedAt = new Date().getTime();
     } else {
         body.completed = false;
         body.completedAt = null;
     }
-    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=>{
-        if(!todo){
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+        if (!todo) {
             return res.status(404).send();
         }
 
-        res.send({todo});
+        res.send({ todo });
     }).catch((e) => {
         res.status(400).send();
     })
@@ -92,21 +93,26 @@ app.patch('/todos/:id', (req,res) => {
 
 
 app.post('/users', (req, res) => {
-  var body = _.pick(req.body, ['email', 'password']);
-  var user = new User(body);
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
 
-  user.save().then(() => {
-      return user.generateAuthToken();
-  }).then((token)=> {
-      res.header('x-auth', token).send(user);
-  }).catch((e) => {
-    res.status(400).send(e);
-  })
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
 });
 
 
-app.listen(port, ()=>{
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+})
+
+
+app.listen(port, () => {
     console.log('Started on port ' + port);
 });
 
-module.exports = {app};
+module.exports = { app };
